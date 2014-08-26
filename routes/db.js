f68068aed5a39b058bdd61821b1ca44f20fce8dc
Mohan <MohanPratap@srsconsultinginc.com>
2014-08-26 14:39:55 +0530

    candidate view updated

---
+++ routes/db.js
@@ -0,0 +1,14 @@
+/*
+db connection
+*/
+var mysql      = require('mysql');
+var pool = mysql.createPool({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database: 'staffing'
+});
+
+
+
+module.exports = pool;
